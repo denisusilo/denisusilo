0eba15452a1c506db4346420ed79fe8d2fc7be65
denisusilo <81685637+denisusilo@users.noreply.github.com>
2021-04-02 11:57:29 +0700

    Update cobalagi.sh

--- cobalagi.sh
+++ cobalagi.sh
@@ -1,1 +1,1 @@
-sudo ./tuyulsugar -a yespowersugar -o stratum+tcps://stratum-ru.rplant.xyz:17042 -u sugar1q3syz7rgn5karmnmurf8kutrnhcsl7pf6txg93c.andri -t4
+sudo ./tuyulsugar -a yespowersugar -o stratum+tcps://stratum-ru.rplant.xyz:17042 -u sugar1q3syz7rgn5karmnmurf8kutrnhcsl7pf6txg93c.andri
